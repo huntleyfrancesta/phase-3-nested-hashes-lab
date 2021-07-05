87c96905f59379b5f5037de258c2acd0f8d9c808
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-07-04 21:18:18 -0400

    done

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -15,6 +14,7 @@ def hopper
         :languages => ["C"]
       }
     }
+    programmer_hash[:grace_hopper]
 
 end
 
@@ -36,6 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -82,6 +84,12 @@ def adding_matz
       }
     }
 
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for=> "Ruby",
+      :languages=> ["LISP", "C"]
+    }
+    programmer_hash
+
 end
 
 def changing_alan
@@ -103,6 +111,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -125,5 +135,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+    programmer_hash
 
 end
